adf2f0dd17e42496075a3b6e0ff9534d1858def3
selen çakıroğlu <selencakiroglu@gmail.com>
2019-05-04 21:48:01 +0300

    Initial commit.

--- AddToCart/Program.cs
+++ AddToCart/Program.cs
@@ -9,10 +9,11 @@ namespace AddToCart
         {
             BuildWebHost(args).Run();
         }
-
+    
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
     }
 }
+
